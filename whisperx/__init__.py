d3ff8c7c86069e9fc9054fd599e92a84d7b9d4f9
Alexandre Richonnier <heralight@users.noreply.github.com>
2024-09-12 09:59:35 +0200

    Update __init__.py

--- whisperx/__init__.py
+++ whisperx/__init__.py
@@ -1,4 +1,4 @@
 from .transcribe import load_model
 from .alignment import load_align_model, align
 from .audio import load_audio
-from .diarize import assign_word_speakers, DiarizationPipeline, DiarizationResult
+from .diarize import assign_word_speakers, DiarizationPipeline, DiarizationResult, Segment
